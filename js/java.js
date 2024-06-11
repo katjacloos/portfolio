e3a23b7bff9d5079f986ab76fb29d534c89d17d7
Katja Cloos <katjacloos@hotmail.dk>
2024-06-11 14:33:08 +0200

    ryddet op i koder

--- js/java.js
+++ js/java.js
@@ -1,12 +1,12 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+// 'NÆSTE KNAP'/'FORRIGE KNAP' KONTROL
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+// BILLEDE KONTROL
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
